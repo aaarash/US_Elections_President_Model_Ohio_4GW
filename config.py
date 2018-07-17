58117c050ce03d3f61954721c83b0d86ef2bf131
Mike Chanhiran <37253820+mplusc@users.noreply.github.com>
2018-07-17 07:47:07 -0400

    Delete config.py

--- config.py
+++
@@ -1,5 +0,0 @@
-# Twitter API Keys
-consumer_key = "3X96TRZOiKG4wgKY6vLfRfEH0"
-consumer_secret = "X6YLBhiSZje3E8sJbvUzjJ9u7kse8enRaM1NS00zj1UYkwa9OX"
-access_token = "282303729-9z7jnZ2ctOdxjUhCP2zlkAdCN0OvXPoGSW46DHIA"
-access_token_secret = "PWaz3U2OkmDoIRi1QtKLkir1U7tizNWYJwpYzvsxU1Ri0"
